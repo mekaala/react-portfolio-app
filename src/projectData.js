2f6cdd832fd0f2b2629544e95b1d7b771e81fabb
mekaala <mekaala@gmail.com>
2020-04-16 13:39:08 -0400

    added links to repositories

--- src/projectData.js
+++ src/projectData.js
@@ -3,24 +3,28 @@ export default [
         name: 'Dynamic Jeopardy',
         url: 'https://nintendo-jeopardy.netlify.com/',
         image: require('./images/Jeopardy.jpg'),
-        description: 'Created a Jeopardy board utilizing front-end development skills earned from HTML, CSS, and Javascript experience.'
+        description: 'Created a Jeopardy board utilizing front-end development skills earned from HTML, CSS, and Javascript experience.',
+        githubUrl: 'https://github.com/mekaala/Dynamic-Jeopardy',
     },
     {
         name: 'Atlanta Photography Database',
         url: 'https://mekaala-atl-photos.herokuapp.com/',
         image: require('./images/ATLPhoto.jpg'),
-        description: 'Utilized full-stack development by creating a photography database where users can create, read, update, and delete regions, areas, and photographers of Atlanta.'
+        description: 'Utilized full-stack development by creating a photography database where users can create, read, update, and delete regions, areas, and photographers of Atlanta.',
+        githubUrl: 'https://github.com/mekaala/project-two',
     },
     {
         name: 'Mindify',
         url: 'https://mekaala-mindify-app.herokuapp.com/',
         image: require('./images/MindifyApp.jpg'),
-        description: 'Developed a full stack application with React.js and Node.js on stress management where users can create, read, update, and delete stressful environments, relaxation exercises, and focus exercises.'
+        description: 'Developed a full stack application with React.js and Node.js on stress management where users can create, read, update, and delete stressful environments, relaxation exercises, and focus exercises.',
+        githubUrl: 'https://github.com/mekaala/project-three',
     },
     {
         name: 'Blathering',
         url: 'https://mekaala-blathering-app.herokuapp.com/',
         image: require('./images/BlatheringApp.jpg'),
-        description: 'Developed with Python, Django, and React. Database of the video game Animal Crossing that helps users discover obtainable fish, bugs, and fossils.'
+        description: 'Developed with Python, Django, and React. Database of the video game Animal Crossing that helps users discover obtainable fish, bugs, and fossils.',
+        githubUrl: 'https://github.com/mekaala/project-four',
     },
 ];
