1a1682fe7718858366540268262728ad34445aad
mekaala <mekaala@gmail.com>
2020-04-16 13:47:07 -0400

    added GA profile URL

--- src/components/SkillList.jsx
+++ src/components/SkillList.jsx
@@ -12,7 +12,8 @@ export default class SkillList extends Component {
                 location: 'Atlanta, Georgia',
                 title: 'Software Engineering Fellow',
                 date: 'January 2020 to April 2020',
-                description: '12-week, 500+ hour training program focused on developing web applications utilizing software such as Node.js, React, Python, and Django.'
+                description: '12-week, 500+ hour training program focused on developing web applications utilizing software such as Node.js, React, Python, and Django.',
+                url: 'https://profiles.generalassemb.ly/profiles/mekaal-ahmad',
             }
         ],
         showResume: false
@@ -50,6 +51,7 @@ export default class SkillList extends Component {
                                 <h3>{ rel.title }</h3>
                                 <h4>{ rel.date }</h4>
                                 <p>{ rel.description }</p>
+                                <a href={ rel.url } target="_blank">General Assembly Profile</a>
                             </div>
                         ))}
                     </div>
