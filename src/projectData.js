ac4bfeb68a948d461f2bfe507942acf5059bc4d3
mekaala <mekaala@gmail.com>
2020-04-16 12:20:22 -0400

    replaced png photos with smaller size photos

--- src/projectData.js
+++ src/projectData.js
@@ -2,25 +2,25 @@ export default [
     {
         name: 'Dynamic Jeopardy',
         url: 'https://nintendo-jeopardy.netlify.com/',
-        image: require('./images/DynamicJeopardy.png'),
+        image: require('./images/Jeopardy.jpg'),
         description: 'Created a Jeopardy board utilizing front-end development skills earned from HTML, CSS, and Javascript experience.'
     },
     {
         name: 'Atlanta Photography Database',
         url: 'https://mekaala-atl-photos.herokuapp.com/',
-        image: require('./images/PhotoDatabase.png'),
+        image: require('./images/ATLPhoto.jpg'),
         description: 'Utilized full-stack development by creating a photography database where users can create, read, update, and delete regions, areas, and photographers of Atlanta.'
     },
     {
         name: 'Mindify',
         url: 'https://mekaala-mindify-app.herokuapp.com/',
-        image: require('./images/Mindify.png'),
+        image: require('./images/MindifyApp.jpg'),
         description: 'Developed a full stack application with React.js and Node.js on stress management where users can create, read, update, and delete stressful environments, relaxation exercises, and focus exercises.'
     },
     {
         name: 'Blathering',
         url: 'https://mekaala-blathering-app.herokuapp.com/',
-        image: require('./images/Blathering.png'),
+        image: require('./images/BlatheringApp.jpg'),
         description: 'Developed with Python, Django, and React. Database of the video game Animal Crossing that helps users discover obtainable fish, bugs, and fossils.'
     },
 ];
