3fbcb15bce9b7173f64583c998ce32a3e276f6a4
mekaala <mekaala@gmail.com>
2020-07-29 15:47:14 -0400

    added sticky navigation bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,24 @@
 import React, { Component } from 'react'
 
 export default class NavBar extends Component {
+    componentDidMount() {
+        window.addEventListener('scroll', () => {
+            const isTop = window.scrollY > 1;
+            const navbar = document.getElementById("navbar");
+            if (isTop) {
+                navbar.classList.add('scrolled');
+            } else {
+                navbar.classList.remove('scrolled');
+            }
+        })
+    }
+    componentWillUnmount() {
+        window.removeEventListener('scroll');
+    }
     render() {
         return (
-            <div>
-                <nav className="navbar">
+            <div className="navbar" id="navbar">
+                <nav>
                     <a href="#about">ABOUT</a>
                     <a href="#skills">SKILLS</a>
                     <a href="#home"><h1>Mekaal Ahmad</h1></a>
