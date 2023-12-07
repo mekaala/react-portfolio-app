558ab00b42939dd148cbad0dbc95f9ea4fcafd61
mekaala <mekaala@gmail.com>
2023-12-07 16:04:51 -0500

    created a Home file in order to utilize react router

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import SplashPage from './SplashPage'
+import SkillList from './SkillList'
+import ProjectList from './ProjectList'
+
+function Home() {
+  return (
+    <div>
+        <SplashPage />
+        {/* <SkillList /> */}
+    </div>
+  )
+}
+
+export default Home
